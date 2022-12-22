f8ec9b3260683f9abe9b6de9917242c0482b04ca
Konrad <kowalczykkon@protonmail.com>
2022-12-22 21:11:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import requests
+import board
+import adafruit_dht
+import time
+# Initial the dht device, with data pin connected to:
+# dhtDevice = adafruit_dht.DHT22(board.D4)
+ 
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
+dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+ 
+while True:
+    try:
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature;
+        temperature_f = temperature_c * (9 / 5) + 32;
+        humidity = dhtDevice.humidity;
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        );
+        
+        url = 'https://api.thingspeak.com/update?api_key=YVFPSOWJ8WF64X2G&field1=0';
+    
+        sensor_readings = {'field1': temperature_f, 'field2': temperature_c, 'field3': humidity};
+        req_header = {'Content-type': 'application/json'};
+        res = requests.post(url, sensor_readings, req_header);
+        print(f"Print: {res.text}");
+        print(f"Status code: {res.status_code}");
+ 
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+ 
+    time.sleep(50.0)
+ 
